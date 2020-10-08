91a462b28e9fd4ec323145c23eba3684fbefbd87
Vasiliy Vanchuk <v.vanchuk@tut.by>
2020-10-08 22:49:14 +0300

    Setup linting (#2)
    * Setup eslint
    
    * Setup prettier
    
    * Add node plugin for eslint
    
    * Setup lint-staged
    
    * Status code 0 for absent tests
    
    * Add workflow
    
    * Cleanup package-lock.json

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.get('/health', (req, res) => {
+app.get("/health", (req, res) => {
   res.json({
-    status: "OK"
+    status: "OK",
   });
 });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`App is listening at http://localhost:${port}`);
 });
